e38cfd2357bfdd1fabdac91e553eeb7da32a65d6
Hlib1234 <yefimenkohlib@gmail.com>
2025-10-04 11:33:12 +0300

    commentрhkckgf

--- mine.py
+++ mine.py
@@ -1,1 +1,8 @@
-print("hellow")
+import pygame
+import random
+
+pygame.init()
+screen_width = 640
+screen_height = 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display,set_caption("Лфбіринт")
