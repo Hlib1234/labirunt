0b718d31261bb1235a2ab9f2d5fb13c8cd34f237
Hlib1234 <yefimenkohlib@gmail.com>
2025-10-04 14:55:25 +0300

    commentрhkckgf

--- mine.py
+++ mine.py
@@ -17,3 +17,17 @@ line_offset = 20
 line_gap = 40
 door_width = 20
 door_gap = 40
+max_openings_per_line = 5
+
+player_radius = 10
+player_speed = 5
+player_x = screen_width - 12
+player_y = screen_height - line_offset
+lines = []
+for i in range(0, screen_width, line_gap):
+    rect = pygame.Rect(i,0,line_whidth, screen_height)
+    num_openings = random.randint(1,max_openings_per_line)
+    if num_openings == 1:
+        door_pos = random.randint(line_offset + door_width, screen_height - line_offset - door_width)
+        lines.append(pygame.Rect(i,0,line_whidth,door_pos - door_width))
+        lines,append(pygame.Rect(i,door_pos + door_width,line_whidth,screen_height - door ))
