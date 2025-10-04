e36f0f35924f74f03f5731edd0117df937f475c5
Hlib1234 <yefimenkohlib@gmail.com>
2025-10-04 12:08:59 +0300

    commentрhkckgf

--- mine.py
+++ mine.py
@@ -5,4 +5,15 @@ pygame.init()
 screen_width = 640
 screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display,set_caption("Лфбіринт")
+pygame.display.set_caption("Лфбіринт")
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+
+line_whidth = 10
+line_offset = 20
+line_gap = 40
+door_width = 20
+door_gap = 40
